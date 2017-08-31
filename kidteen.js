ae7e41638342e2fbc12149626717dc832ac75b6b
mylesasc4 <mylesduah123@gmail.com>
2017-08-30 23:22:20 -0400

    new

--- kidteen.js
+++ kidteen.js
@@ -1,20 +1,20 @@
-function onSignIn(googleUser) {
-    console.log('User signed in!');
-    var profile = googleUser.getBasicProfile();
-    //change userName text, img source, & email text based on profile
-    $(".userName").text(profile.getName());
-    $("#pfp").attr("src", profile.getImageUrl());
-    $(".email").text(profile.getEmail());
-}
+// function onSignIn(googleUser) {
+//     console.log('User signed in!');
+//     var profile = googleUser.getBasicProfile();
+//     //change userName text, img source, & email text based on profile
+//     $(".userName").text(profile.getName());
+//     $("#pfp").attr("src", profile.getImageUrl());
+//     $(".email").text(profile.getEmail());
+// }
 
-function onSignOut() {
-    //should sign user out and toggleHidden
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-        console.log('User signed out.')
-        //setting back to default
-        $(".userName").text("USER_NAME");
-        $("#pfp").attr("src", "assets/logo.png");
-        $(".email").text("example@example.com");
-    });
-}
+// function onSignOut() {
+//     //should sign user out and toggleHidden
+//     var auth2 = gapi.auth2.getAuthInstance();
+//     auth2.signOut().then(function () {
+//         console.log('User signed out.')
+//         //setting back to default
+//         $(".userName").text("USER_NAME");
+//         $("#pfp").attr("src", "assets/logo.png");
+//         $(".email").text("example@example.com");
+//     });
+// }
